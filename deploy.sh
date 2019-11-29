d3073f8e18ea1a32a91ea71480ff1ba668554963
Christian Brevik <christian.brevik@gmail.com>
2019-11-29 19:41:27 +0100

    Add docs for deploy

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-# create resource group with app name
-az group create --location $2 --name $1 --query "properties.provisioningState"
-
-# create resources
-az group deployment create -g $1 --template-file azuredeploy.json --parameters appName=$1 location=$2 serverFarmId=$3 --query "properties.provisioningState"
